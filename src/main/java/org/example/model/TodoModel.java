d40dd2caca4c45d7f69a4c27822842af49b35164
ksg0605 <ksg0605@gmail.com>
2022-02-13 13:32:35 +0900

    Refactoring

--- src/main/java/org/example/model/TodoModel.java
+++ src/main/java/org/example/model/TodoModel.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class TodoEntity {
+public class TodoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
